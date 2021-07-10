cad07399d9bbf5d31c00ad354623aaa2e25a05e2
steven-rr <steverelativity@gmail.com>
2021-07-10 02:55:55 -0400

    confirming that transformer goes thru training OK.

--- assignment4/main_steve.py
+++ assignment4/main_steve.py
@@ -176,31 +176,121 @@ d3 = torch.load('./data/d3.pt')
 d4 = torch.load('./data/d4.pt')
 
 
+# Define the maximum length of the sentence. Shorter sentences will be padded to that length and longer sentences will be croped. Given that the average length of the sentence in the corpus is around 13, we can set it to 20
+MAX_LEN = 20
 
-inputs = train_inxs[0:2]
-inputs = torch.LongTensor(inputs)
+# Define the source and target language
+SRC = Field(tokenize = "spacy",
+            tokenizer_language="de",
+            init_token = '<sos>',
+            eos_token = '<eos>',
+            fix_length = MAX_LEN,
+            lower = True)
 
-model = TransformerTranslator(input_size=len(word_to_ix), output_size=2, device=device, hidden_dim=128, num_heads=2, dim_feedforward=2048, dim_k=96, dim_v=96, dim_q=96, max_length=train_inxs.shape[1])
+TRG = Field(tokenize = "spacy",
+            tokenizer_language="en",
+            init_token = '<sos>',
+            eos_token = '<eos>',
+            fix_length = MAX_LEN,
+            lower = True)
 
-embeds = model.embed(inputs)
-# attention
-hidden_states = model.multi_head_attention(embeds)
+# Download and split the data. It should take some time
+train_data, valid_data, test_data = Multi30k.splits(exts = ('.de', '.en'),
+                                                    fields = (SRC, TRG))
+# Define Batchsize
+BATCH_SIZE = 128
 
-## feed forward
-outputs = model.feedforward_layer(hidden_states)
+# Build the vocabulary associated with each language
+SRC.build_vocab(train_data, min_freq = 2)
+TRG.build_vocab(train_data, min_freq = 2)
 
-# final layer.
-scores = model.final_layer(outputs)
+# Get the padding index to be ignored later in loss calculation
+PAD_IDX = TRG.vocab.stoi['<pad>']
+# Get data-loaders using BucketIterator
+train_loader, valid_loader, test_loader = BucketIterator.splits(
+    (train_data, valid_data, test_data),
+    batch_size = BATCH_SIZE, device = device)
 
-inputs = train_inxs[0:2]
-inputs = torch.LongTensor(inputs)
+# Get the input and the output sizes for model
+input_size = len(SRC.vocab)
+output_size = len(TRG.vocab)
 
-outputs = model.forward(inputs)
+# Hyperparameters
+learning_rate = 1e-3
+EPOCHS = 10
 
-try:
-    print("Difference:", torch.sum(torch.pairwise_distance(outputs, scores)).item()) # should be very small (<3e-5)
-except:
-    print("NOT IMPLEMENTED")
+# Model
+trans_model = TransformerTranslator(input_size, output_size, device, max_length = MAX_LEN).to(device)
+
+# optimizer = optim.Adam(model.parameters(), lr = learning_rate)
+optimizer = torch.optim.Adam(trans_model.parameters(), lr=learning_rate)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer)
+criterion = nn.CrossEntropyLoss(ignore_index=PAD_IDX)
+for epoch_idx in range(EPOCHS):
+    print("-----------------------------------")
+    print("Epoch %d" % (epoch_idx + 1))
+    print("-----------------------------------")
+
+    train_loss, avg_train_loss = train(trans_model, train_loader, optimizer, criterion)
+    scheduler.step(train_loss)
+
+    val_loss, avg_val_loss = evaluate(trans_model, valid_loader, criterion)
+
+    avg_train_loss = avg_train_loss.item()
+    avg_val_loss = avg_val_loss.item()
+    print("Training Loss: %.4f. Validation Loss: %.4f. " % (avg_train_loss, avg_val_loss))
+    print("Training Perplexity: %.4f. Validation Perplexity: %.4f. " % (np.exp(avg_train_loss), np.exp(avg_val_loss)))
+######################################### TRANSFORMER UNIT TEST ##################################################################
+
+# train_inxs = np.load('./data/train_inxs.npy')
+# val_inxs = np.load('./data/val_inxs.npy')
+# train_labels = np.load('./data/train_labels.npy')
+# val_labels = np.load('./data/val_labels.npy')
+#
+# # load dictionary
+# word_to_ix = {}
+# with open("./data/word_to_ix.csv", "r") as f:
+#     reader = csv.reader(f)
+#     for line in reader:
+#         word_to_ix[line[0]] = line[1]
+# print("Vocabulary Size:", len(word_to_ix))
+#
+# print(train_inxs.shape)  # 7000 training instances, of (maximum/padded) length 43 words.
+# print(val_inxs.shape)  # 1551 validation instances, of (maximum/padded) length 43 words.
+# print(train_labels.shape)
+# print(val_labels.shape)
+#
+# d1 = torch.load('./data/d1.pt')
+# d2 = torch.load('./data/d2.pt')
+# d3 = torch.load('./data/d3.pt')
+# d4 = torch.load('./data/d4.pt')
+#
+#
+#
+# inputs = train_inxs[0:2]
+# inputs = torch.LongTensor(inputs)
+#
+# model = TransformerTranslator(input_size=len(word_to_ix), output_size=2, device=device, hidden_dim=128, num_heads=2, dim_feedforward=2048, dim_k=96, dim_v=96, dim_q=96, max_length=train_inxs.shape[1])
+#
+# embeds = model.embed(inputs)
+# # attention
+# hidden_states = model.multi_head_attention(embeds)
+#
+# ## feed forward
+# outputs = model.feedforward_layer(hidden_states)
+#
+# # final layer.
+# scores = model.final_layer(outputs)
+#
+# inputs = train_inxs[0:2]
+# inputs = torch.LongTensor(inputs)
+#
+# outputs = model.forward(inputs)
+#
+# try:
+#     print("Difference:", torch.sum(torch.pairwise_distance(outputs, scores)).item()) # should be very small (<3e-5)
+# except:
+#     print("NOT IMPLEMENTED")
 
 
 
